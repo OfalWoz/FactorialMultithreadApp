a440d0c2c3e64ecb7237de59e181e4ad75c0a7b4
OfalWoz <lsk.craft@gmail.com>
2022-01-16 15:40:37 +0100

    Added calculating on threads

--- app/src/main/java/pl/ofalwoz/factorialmultithreadapp/MainActivity.java
+++ app/src/main/java/pl/ofalwoz/factorialmultithreadapp/MainActivity.java
@@ -1,8 +1,11 @@
 package pl.ofalwoz.factorialmultithreadapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -12,10 +15,12 @@ import java.math.BigInteger;
 
 public class MainActivity extends AppCompatActivity {
 
-    BigInteger result;
-    TextView showResult;
-    TextView insertNumber;
-    int Number, i, n;
+    private static final String TAG = "MainActivity";
+    public BigInteger Iloczyn;
+    public TextView showResult;
+    public TextView insertNumber;
+    public int Number, i, n;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,14 +31,90 @@ public class MainActivity extends AppCompatActivity {
         showResult = findViewById(R.id.showresult);
 
         showResult.setText("");
+
+        if(savedInstanceState != null){
+            i = savedInstanceState.getInt("counter_state");
+            Number = savedInstanceState.getInt("number");
+        }
     }
 
     public void Count(View view) {
+        final Handler handler = new Handler();
+
         Number = Integer.parseInt(insertNumber.getText().toString());
-        BigInteger iloczyn = BigInteger.valueOf(1);
-        for (int i=1; i<=Number; i++) {
-           iloczyn = iloczyn.multiply(BigInteger.valueOf(i));
+
+        Runnable runnable = new Runnable() { //obliczenia na jednym watku
+            @Override
+            public void run() {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        showResult.setText("Calculating.");
+                    }
+                });
+                Iloczyn = BigInteger.valueOf(1);
+                for (i=1; i<=Number; i++) {
+                    Iloczyn = Iloczyn.multiply(BigInteger.valueOf(i));
+                    Log.d(TAG, "Thread number: "+ Thread.currentThread().getName());
+                }
+                synchronized (this){
+                    try{
+                        wait( Number);
+                    } catch (InterruptedException e){
+                        e.printStackTrace();
+                    }
+                }
+                Log.d(TAG,"Number: "+ Iloczyn);
+
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        showResult.setText(String.valueOf(Iloczyn));
+                    }
+                });
+            }
+        };
+
+        Log.d(TAG, "Thred name 1 "+ Thread.currentThread().getName());
+        if(Number <= 20) {
+            Thread thread = new Thread(runnable);
+            thread.start();
+        } else {
+            int n = Runtime.getRuntime().availableProcessors(); // liczba dostepnych watkow
+            for (int i = 0; i < n; i++) { //obliczanie na kilku watkach
+                Thread object = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        handler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                showResult.setText("Calculating.");
+                            }
+                        });
+                        Iloczyn = BigInteger.valueOf(1);
+                        for (int j=1; j<=Number; j++) {
+                            Iloczyn = Iloczyn.multiply(BigInteger.valueOf(j));
+                            Log.d(TAG, "Thread number: "+ Thread.currentThread().getName());
+                        }
+                        synchronized (this){
+                            try{
+                                wait(Number);
+                            } catch (InterruptedException e){
+                                e.printStackTrace();
+                            }
+                        }
+                        Log.d(TAG,"Number: "+ Iloczyn);
+
+                        handler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                showResult.setText(String.valueOf(Iloczyn));
+                            }
+                        });
+                    }
+                });
+                object.start();
+            }
         }
-        showResult.setText(String.valueOf(iloczyn));
     }
 }
